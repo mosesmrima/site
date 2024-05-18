fcff8a77174915b58e6facf007f679f987994ba0
mrima <mrimamss@gmail.com>
2024-05-19 01:20:56 +0300

    shift from deprecated src

--- src/app/(tabs)/_layout.jsx
+++ src/app/(tabs)/_layout.jsx
@@ -63,7 +63,7 @@ export default function TabLayout() {
                         tabBarIcon: () => (      <Avatar circular size="$2">
                             <Avatar.Image
                                 accessibilityLabel="Nate Wienert"
-                                src="https://images.unsplash.com/photo-1531384441138-2736e62e0919?&w=100&h=100&dpr=2&q=80"
+                                source="https://images.unsplash.com/photo-1531384441138-2736e62e0919?&w=100&h=100&dpr=2&q=80"
                             />
                             <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
                         </Avatar>),
