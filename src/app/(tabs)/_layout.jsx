e4d990a378fe6791574bf5ad8c0fa40bdde84387
mosesmrima <mrimamss@gmail.com>
2024-05-30 21:38:37 +0300

    optimize firebase reads

--- src/app/(tabs)/_layout.jsx
+++ src/app/(tabs)/_layout.jsx
@@ -19,99 +19,99 @@ export default function TabLayout() {
 
     if (currentUser) {
         return (
-                <Tabs sceneContainerStyle={{backgroundColor: "white"}}   screenOptions={({ route }) => ({
-                    tabBarActiveTintColor: constants.colours.primary,
-                    tabBarStyle: {
-                        display: route.name === "login" || route.name === "create" ? "none" : "flex"
-                    },
-                    tabBarShowLabel: false
-                })}>
-                    <Tabs.Screen
-                        name="index"
-                        options={{
-                            title: 'Home',
-                            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-                        }}
-                    />
+            <Tabs sceneContainerStyle={{backgroundColor: "white"}}   screenOptions={({ route }) => ({
+                tabBarActiveTintColor: constants.colours.primary,
+                tabBarStyle: {
+                    display: route.name === "login" || route.name === "create" ? "none" : "flex"
+                },
+                tabBarShowLabel: false
+            })}>
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: 'Home',
+                        tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name="add"
-                        options={{
-                            title: 'New Post',
-                            tabBarIcon: ({ color }) => <FontAwesome size={28} name="plus-square" color={color} />,
-                        }}
-                    />
+                <Tabs.Screen
+                    name="add"
+                    options={{
+                        title: 'New Post',
+                        tabBarIcon: ({ color }) => <FontAwesome size={28} name="plus-square" color={color} />,
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name={"videos"}
-                        options={{
-                            title: 'Videos',
-                            tabBarIcon: ({ color }) => (<FontAwesome size={28} name="video-camera" color={color} />),
-                        }}
-                    />
+                <Tabs.Screen
+                    name={"videos"}
+                    options={{
+                        title: 'Videos',
+                        tabBarIcon: ({ color }) => (<FontAwesome size={28} name="video-camera" color={color} />),
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name="chat"
-                        options={{
-                            title: 'Messages',
-                            headerShown: false,
-                            tabBarIcon: ({ color }) => <MaterialIcons size={28} name="message" color={color} />,
-                        }}
-                    />
+                <Tabs.Screen
+                    name="chat"
+                    options={{
+                        title: 'Messages',
+                        headerShown: false,
+                        tabBarIcon: ({ color }) => <MaterialIcons size={28} name="message" color={color} />,
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name="users"
-                        options={{
-                            title: 'Messages',
-                            headerShown: false,
-                            href: null,
-                        }}
-                    />
+                <Tabs.Screen
+                    name="users"
+                    options={{
+                        title: 'Messages',
+                        headerShown: false,
+                        href: null,
+                    }}
+                />
 
 
-                    <Tabs.Screen
-                        name={"profile"}
-                        options={{
-                            title: 'My Profile',
-                            tabBarIcon: () => (      <Avatar circular size="$2">
-                                <Avatar.Image
-                                    accessibilityLabel="Nate Wienert"
-                                    source={currentUser.profilePic? currentUser.profilePic : defaultAvatar}
-                                />
-                                <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
-                            </Avatar>),
-                        }}
-                    />
+                <Tabs.Screen
+                    name={"profile"}
+                    options={{
+                        title: 'My Profile',
+                        tabBarIcon: () => (      <Avatar circular size="$2">
+                            <Avatar.Image
+                                accessibilityLabel="Nate Wienert"
+                                source={currentUser.profilePic? currentUser.profilePic : defaultAvatar}
+                            />
+                            <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
+                        </Avatar>),
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name={"login"}
-                        options={{
-                            href: null,
-                            headerShown: false,
-                        }}
-                    />
+                <Tabs.Screen
+                    name={"login"}
+                    options={{
+                        href: null,
+                        headerShown: false,
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name={"create"}
-                        options={{
-                            title: 'Create Account',
-                            href: null,
-                            headerShown: false,
-                        }}
-                    />
+                <Tabs.Screen
+                    name={"create"}
+                    options={{
+                        title: 'Create Account',
+                        href: null,
+                        headerShown: false,
+                    }}
+                />
 
-                    <Tabs.Screen
-                        name={"usersProfile"}
-                        options={{
-                            title: 'Users Profile',
-                            href: null,
-                        }}
-                    />
-                </Tabs>
+                <Tabs.Screen
+                    name={"usersProfile"}
+                    options={{
+                        title: 'Users Profile',
+                        href: null,
+                    }}
+                />
+            </Tabs>
         );
     } else {
         return <View style={{flex: 1, justifyContent: "center", alignItems: "center"} }>
-                <Text>Exceeded firebase free tier, try after 24hrs </Text>
+            <Text>Loading Please wait...</Text>
         </View>
     }
 }
